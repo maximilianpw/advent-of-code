18596893b945d3bcf0020adf16a978b652897fc7
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-12-07 19:43:34 +0100

    init day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file: String = fs::read_to_string("init-input.txt").expect("Unable to read file");
+    let item_ranges: Vec<&str> = file.split(",").collect();
+    let mut total: i32 = 0;
+
+    for item in item_ranges {}
 }
